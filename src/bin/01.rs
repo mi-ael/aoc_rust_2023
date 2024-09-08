9f97b616ec9eff1aa4803b060a75e6ce8e724987
mi-ael <miael.oss.1970@gmail.com>
2024-09-08 10:24:48 +0200

    01b

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -19,7 +17,15 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-  None
+  Some(
+    input
+      .lines()
+      .map(|line| {
+        first_last_number_spelled(line).unwrap_or_else(|| panic!("Invalid line: {}", line))
+      })
+      .map(|(first, last)| first * 10 + last)
+      .sum::<u32>(),
+  )
 }
 
 trait FirstLast: DoubleEndedIterator {
@@ -34,6 +40,49 @@ trait FirstLast: DoubleEndedIterator {
   }
 }
 impl<I: DoubleEndedIterator> FirstLast for I {}
+
+fn first_last_number_spelled(line: &str) -> Option<(u32, u32)> {
+  let spelled_out_digits = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+  ];
+  let mut line_slice = &line[..];
+
+  let first = 'outer: loop {
+    if let Some(digit) = line_slice.chars().next().unwrap().to_digit(10) {
+      break digit;
+    }
+
+    for (i, &digits) in spelled_out_digits.iter().enumerate() {
+      if line_slice.starts_with(digits) {
+        break 'outer (i + 1) as u32;
+      }
+    }
+
+    line_slice = &line_slice[1..];
+    if line_slice.is_empty() {
+      return None;
+    }
+  };
+
+  let last = 'outer: loop {
+    if let Some(digit) = line_slice.chars().next_back().unwrap().to_digit(10) {
+      break digit;
+    }
+
+    for (i, digits) in spelled_out_digits.iter().enumerate() {
+      if line_slice.ends_with(digits) {
+        break 'outer (i + 1) as u32;
+      }
+    }
+
+    line_slice = &line_slice[..line_slice.len() - 1];
+    if line_slice.is_empty() {
+      return None;
+    }
+  };
+
+  Some((first, last))
+}
 
 #[cfg(test)]
 mod tests {
